b0f4c86be060c0de9317ef8960212203ee2011ba
fahimprito <ahmmedahmmed75@gmail.com>
2024-09-22 19:23:03 +0600

    setup and assets

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                lightGreen: '#B4F461',
+            },
+            fontFamily: {
+                lexend: "Lexend",
+            },
+        },
+    },
+    plugins: [],
+}
