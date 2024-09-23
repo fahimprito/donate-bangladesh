4b0c5ac396c525a965d39117f60bbb7356253550
fahimprito <ahmmedahmmed75@gmail.com>
2024-09-23 17:45:36 +0600

    blog nav

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,5 @@
-document.getElementById('blog-page').addEventListener("click", function () {
-    
-    window.location.href = "../blog.html";
 
+document.getElementById('home-page').addEventListener("click", function () {
+    window.location.href = "../index.html";
     
 })
